b4c943217fa84b189d61b0161417ccfa17abd7f9
wpcodevo <ziddah.edem@gmail.com>
2021-10-10 13:12:09 +0000

    added more images

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
-console.log("hello");
+/* Toogle Nav Menu */
+const hamburger = document.querySelector(".hamburger");
+const navbar = document.querySelector(".navbar");
+
+hamburger.addEventListener("click", () => {
+  navbar.classList.toggle("show");
+});
+
+window.addEventListener("scroll", () => {
+  navbar.classList.remove("show");
+});
+
+/* Testimonial Slider */
+const swiper = new Swiper(".myslider", {
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
+  },
+  loop: true,
+  autoHeight: true,
+  grabCursor: true,
+  slidesPerView: "auto",
+  navigation: {
+    nextEl: ".custom-btn ion-icon, .custom-btn",
+  },
+});
