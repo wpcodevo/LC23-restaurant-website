731c4f03b27e6212308f0eeedf92c1bafe9e09c7
wpcodevo <ziddah.edem@gmail.com>
2021-10-19 22:19:02 +0000

    added scroll reveal

--- js/main.js
+++ js/main.js
@@ -24,3 +24,51 @@ const swiper = new Swiper(".myslider", {
     nextEl: ".custom-btn ion-icon, .custom-btn",
   },
 });
+
+/* Scroll Reveal */
+const scroll = ScrollReveal({
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+  reset: true,
+});
+
+scroll.reveal(`.home .col:last-child`, { delay: 600 });
+scroll.reveal(`.icons .icon, .about .col:first-child, .menu .col`, {
+  origin: "bottom",
+  interval: 100,
+});
+
+scroll.reveal(
+  `.home .col:first-child h1, .home .col:first-child .btn,.categories .col:first-child`,
+  {
+    origin: "left",
+    interval: 150,
+    delay: 600,
+  }
+);
+
+scroll.reveal(
+  `.about .col:last-child h2, .about .col:last-child p, .about .col:last-child .btn,.categories .col:last-child`,
+  {
+    origin: "right",
+    interval: 100,
+  }
+);
+
+scroll.reveal(`.categories .col:nth-child(2), .menu .btn, .contact .col`, {
+  origin: "top",
+});
+
+scroll.reveal(`.testimonial .col:first-child`, {
+  origin: "left",
+});
+
+scroll.reveal(`.testimonial .col:last-child`, {
+  origin: "right",
+});
+
+scroll.reveal(`.footer .col div`, {
+  origin: "top",
+  interval: 100,
+});
